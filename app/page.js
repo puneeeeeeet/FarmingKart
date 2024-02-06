864f7ca26431e8e0deeab486fc9a4c38a3ae2d54
puneeeeeeet <puneetchhabra1607@gmail.com>
2024-02-07 02:32:42 +0530

    nav bar completed

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 import Nav from "@/components/navbar/nav";
 export default function Home() {
   return (
-    <div className="bg-white min-h-screen">
+    <div className="bg-white  min-h-screen">
     <Nav></Nav>
-  
+  <div className="flex-grow bg-black"></div>
     </div>
   );
 }
--- components/navbar/nav.js
+++ components/navbar/nav.js
@@ -4,56 +4,73 @@ import { Kart } from "../icons/kart";
 import { Search } from "../icons/search";
 import { User } from "../icons/user";
 import { WishList } from "../icons/wishList";
-export default function Nav(){
-    return(
-        <div className="bg-[#72B462] h-28 w-screen flex flex-col">
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../logo/logo.jpg";
+// import { Home } from "../icons/home";
+// import { Categories } from "../icons/categories";
+// import { TopBrands } from "../icons/topBrands";
 
-            <div className="flex pt-4 pb-5 gap-2 items-center justify-center text-white">
-             <div className="text-2xl font-bold  px-10  ">
-                Farmers Point
-             </div>
-             <div className="border-2 h-8 w-96 rounded-md ">
-                <div className="ml-2 mt-0.5 gap-2 flex items-center">
-                <Icon icon={Search} size="lessSmall" />
-                Search Your Items Here!
-                </div>
-                
-             </div>
-             <div className="ml-5">
-             <Icon icon={WishList} size="medium"/>
-             </div>
-             <div className="ml-2">
-                <Icon icon={Kart} size="large"/>
-             </div>
-            <div className="flex gap-2 ml-5">
-                <div>
-                    User
-                </div>
-                <div>
-                    <Icon icon={User} />
-                </div>
-            </div>
-            </div>
-            
-            <div className="w-screen bg-[#549744] h-12 flex justify-center	font-bold items-center gap-6 text-white">
-            <div>
-                Home
-            </div>
-            <div className="flex justify-center items-center">
-                Categories
-                <Icon icon={ArrowDown}/>
-            </div>
-            <div className="flex justify-center items-center">
-               Top Brands
-               <Icon icon={ArrowDown}/>
-            </div>
-            <div>
-                About
-            </div>
-            <div>
-               Contact Us
-            </div>
-            </div>
+export default function Nav() {
+  const inactiveLink = "flex justify-center items-center p-2 ";
+  const activeLink = inactiveLink + "bg-[#720C1A] rounded-md";
+
+  return (
+    <div className="bg-[#72B462]  w-screen flex flex-col">
+      <div className="flex gap-2 items-center justify-center text-white">
+        <Link href={"/"} className=" pb-3 pr-10 mix-blend-darken	 ">
+          <Image src={logo} width="auto" height="55" alt="Logo" />
+        </Link>
+        <div className="border-2 h-8 w-96 rounded-md ">
+          <div className="ml-2 mr-2 mt-0.5 gap-2 flex text-white items-center">
+            <input
+              className="bg-transparent w-full"
+              placeholder="Search youur product here!"
+            />
+            <button>
+              <Icon icon={Search} size="lessSmall" />
+            </button>
+          </div>
         </div>
-    );
+        <button className=" ml-5">
+          <Icon icon={WishList} size="medium" />
+        </button>
+        <button className="ml-2">
+          <Icon icon={Kart} size="large" />
+        </button>
+        <div className="flex gap-2 ml-5">
+          <div>User</div>
+          <div>
+            <Icon icon={User} />
+          </div>
+        </div>
+      </div>
+
+      <div className="w-screen bg-[#549744] h-10 flex justify-center	font-bold items-center gap-6 text-white">
+        <Link href={"/"} className={inactiveLink}>
+          {/* <Icon icon={Home} size="extraSmall" color="white" /> */}
+          Home
+        </Link>
+
+        <Link href={"/categories"} className={activeLink}>
+          {/* <Icon icon={Categories} size="lessSmall" /> */}
+          Categories
+          <Icon icon={ArrowDown} />
+        </Link>
+
+        <Link href={"/"} className={inactiveLink}>
+          {/* <Icon  icon={TopBrands} size="lessSmall"/> */}
+          Top Brands
+          <Icon icon={ArrowDown} />
+        </Link>
+
+        <Link href={"/"} className={inactiveLink}>
+          About
+        </Link>
+        <Link href={"/"} className={inactiveLink}>
+          Contact Us
+        </Link>
+      </div>
+    </div>
+  );
 }
