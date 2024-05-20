fbc462e5b65f8a60fbbd34779204284ed71268aa
puneeeeeeet <puneetchhabra1607@gmail.com>
2024-05-20 22:59:47 +0530

    testing

--- app/(pages)/About/page.js
+++ app/(pages)/About/page.js
@@ -7,7 +7,7 @@ export default function About(){
             <h1 className="text-2xl font-bold font-serif">Welcome to Kisaan24</h1>
             </div>
             <p className="mx-5 font-medium text-l text-justify">
-            We&apos;re passionate about supporting farmers  and gardeners 
+            We're passionate about supporting farmers  and gardeners 
             alike in cultivating thriving landscapes and bountiful  harvests. With a commitment to  quality,
             innovation, and sustainability, we offer a wide range of premium farming products including seeds,
             herbicides, insecticides, fungicides, and growth promoters, carefully curated to meet the diverse
